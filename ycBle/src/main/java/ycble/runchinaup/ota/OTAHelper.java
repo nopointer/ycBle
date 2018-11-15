6026e294e93e8809cf85751b1d8918eb72574ef0
nopointer <857508412@qq.com>
2018-11-15 12:55:23 +0800

    不混淆代码

--- ycBle/src/main/java/ycble/runchinaup/ota/OTAHelper.java
+++ ycBle/src/main/java/ycble/runchinaup/ota/OTAHelper.java
@@ -3,6 +3,7 @@ package ycble.runchinaup.ota;
 import android.content.Context;
 
 import ycble.runchinaup.device.BleDevice;
+import ycble.runchinaup.log.ycBleLog;
 import ycble.runchinaup.ota.absimpl.nordic.DfuHelper;
 import ycble.runchinaup.ota.callback.OTACallback;
 
@@ -21,12 +22,18 @@ public class OTAHelper {
     }
 
     public void startOTA(Context context, String filePath, BleDevice bleDevice, FirmType firmType, OTACallback otaCallback) {
+        ycBleLog.e("startOTA======>");
+        ycBleLog.e("filePath======>" + filePath);
+        ycBleLog.e("bleDevice======>" + bleDevice);
+        ycBleLog.e("firmType======>" + firmType);
+        ycBleLog.e("otaCallback======>" + otaCallback);
         startOTA(context, filePath, bleDevice.getMac(), bleDevice.getName(), firmType, otaCallback);
     }
 
     public void startOTA(Context context, String filePath, String mac, String name, FirmType firmType, OTACallback otaCallback) {
         switch (firmType) {
             case NORDIC:
+            default:
                 DfuHelper.getDfuHelper().start(context, filePath, mac, name, otaCallback);
                 break;
         }
