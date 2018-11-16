b6d51469b9b905b0c5af187f5fe223740eface6b
nopointer <857508412@qq.com>
2018-11-16 11:34:03 +0800

    提交 抽象的dfuService

--- ycBle/src/main/java/ycble/runchinaup/ota/absimpl/nordic/DfuServiceAbs.java
+++ ycBle/src/main/java/ycble/runchinaup/ota/absimpl/nordic/DfuServiceAbs.java
@@ -1,7 +1,15 @@
 package ycble.runchinaup.ota.absimpl.nordic;
 
+import android.app.Activity;
+
 import no.nordicsemi.android.dfu.DfuBaseService;
 
 public abstract class DfuServiceAbs extends DfuBaseService {
 
+
+    @Override
+    protected abstract Class<? extends Activity> getNotificationTarget();
+
+    @Override
+    protected abstract boolean isDebug();
 }
